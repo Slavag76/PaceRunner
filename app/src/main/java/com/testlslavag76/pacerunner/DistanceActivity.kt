37dfcd275d08c6003ae5d7138338121a976a3434
SlavaGr <Slavag76@yandex.ru>
2021-10-26 15:45:57 +0300

    Базовый функционал готов

--- app/src/main/java/com/testlslavag76/pacerunner/DistanceActivity.kt
+++ app/src/main/java/com/testlslavag76/pacerunner/DistanceActivity.kt
@@ -2,10 +2,34 @@ package com.testlslavag76.pacerunner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.testlslavag76.pacerunner.databinding.ActivityDistanceBinding
 
 class DistanceActivity : AppCompatActivity() {
+
+    lateinit var bindingDistance: ActivityDistanceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_distance)
+        bindingDistance = ActivityDistanceBinding.inflate(layoutInflater)
+        setContentView(bindingDistance.root)
+
+        bindingDistance.buttonCalculateDistance.setOnClickListener {
+
+            val editTextHoursFT = bindingDistance.editTextHoursFT.text.toString().toInt()
+            val editTextMinFT = bindingDistance.editTextMinFT.text.toString().toInt()
+            val editTextSecFT = bindingDistance.editTextSecFT.text.toString().toInt()
+
+            val minPaceFromView = bindingDistance.minPaceFromView.text.toString().toInt()
+            val secPaceFromView = bindingDistance.secPaceFromView.text.toString().toInt()
+
+            val totalSecond = editTextHoursFT * 3600 + editTextMinFT * 60 + editTextSecFT
+            val paceSecond = minPaceFromView * 60 + secPaceFromView
+
+            val resultKM = totalSecond / paceSecond
+            val resultMet = totalSecond % paceSecond
+
+            bindingDistance.resultDistance.text = "${resultKM} км ${resultMet} м"
+
+        }
     }
 }
